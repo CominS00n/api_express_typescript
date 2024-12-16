faadea7261d69beac1ff3ffcb53639f62fe09bd2
CominS00n <spuckpoo@gmail.com>
2024-12-16 11:23:01 +0700

    up

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import session from "express-session";
 import cookieParser from "cookie-parser";
-
-// import route from "./route";
+import https from "https";
+import fs from "fs";
 import route from "./routes/index";
 
 dotenv.config();
 
 const app: Express = express();
 const port: number = 8000;
-
 
 app.use(express.json());
 app.use(
@@ -35,6 +34,11 @@ app.use(
 
 app.use("/api/v2", route);
 
-app.listen(port, '0.0.0.0',() => {
+const httpsOptions = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+
+https.createServer(httpsOptions, app).listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
