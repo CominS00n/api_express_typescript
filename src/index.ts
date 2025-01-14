18c7357b4ffe030ec3bbb231a59313786dc8461a
CominS00n <spuckpoo@gmail.com>
2025-01-14 11:55:37 +0700

    fix corsOrigin

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import session from "express-session";
 import cookieParser from "cookie-parser";
-// import https from "https";
-// import fs from "fs";
 import route from "./routes/index";
 
 dotenv.config();
 
 const app: Express = express();
 const port = Number(process.env.PORT) || 8000;
-const corsOrigin = process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000";
-console.log(corsOrigin);
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
+const corsOrigin = process.env.CORS_ORIGIN?.split(",") || ["http://localhost:82"];
+app.use(cors({
     origin: corsOrigin,
     credentials: true,
-  })
-);
+}));
 
 app.use(
   session({
@@ -36,15 +32,6 @@ app.use(
 );
 
 app.use("/api/v2", route);
-
-// const httpsOptions = {
-//   key: fs.readFileSync("server.key"),
-//   cert: fs.readFileSync("server.cert"),
-// };
-
-// https.createServer(httpsOptions, app).listen(port, "0.0.0.0", () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
